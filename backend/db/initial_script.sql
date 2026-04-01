8bd00ddb299d5824f9128348fdca2ba4707cb4a8
Demeter1611 <demeteradrian1611@gmail.com>
2026-04-01 15:20:58 +0300

    update database columns to PascalCase

--- backend/db/initial_script.sql
+++ backend/db/initial_script.sql
@@ -10,45 +10,45 @@ GO
 IF OBJECT_ID('Roles') IS NULL
 BEGIN
     CREATE TABLE Roles(
-        id INT PRIMARY KEY IDENTITY(1,1),
-        name VARCHAR(50) NOT NULL UNIQUE CHECK(name IN ('admin', 'user'))
+        Id INT PRIMARY KEY IDENTITY(1,1),
+        Name VARCHAR(50) NOT NULL UNIQUE CHECK(name IN ('admin', 'user'))
     );
-    INSERT INTO Roles(name) VALUES ('admin'), ('user');
+    INSERT INTO Roles(Name) VALUES ('admin'), ('user');
 END
 
 IF OBJECT_ID('Users') IS NULL
 BEGIN
     CREATE TABLE Users(
-        id INT PRIMARY KEY IDENTITY(1,1),
-        email VARCHAR(250) NOT NULL UNIQUE,
-        fullName VARCHAR(250),
-        password VARCHAR(250),
-        roleId INT FOREIGN KEY REFERENCES Roles(id),
-        location VARCHAR(250)
+        Id INT PRIMARY KEY IDENTITY(1,1),
+        Email VARCHAR(250) NOT NULL UNIQUE,
+        FullName VARCHAR(250),
+        Password VARCHAR(250),
+        RoleId INT FOREIGN KEY REFERENCES Roles(Id),
+        Location VARCHAR(250)
     );
 END
 
 IF OBJECT_ID('DeviceTypes') IS NULL
 BEGIN
     CREATE TABLE DeviceTypes(
-        id INT PRIMARY KEY IDENTITY(1,1),
-        name VARCHAR(50) NOT NULL UNIQUE CHECK(name IN ('phone', 'tablet'))
+        Id INT PRIMARY KEY IDENTITY(1,1),
+        Name VARCHAR(50) NOT NULL UNIQUE CHECK(name IN ('phone', 'tablet'))
     );
-    INSERT INTO DeviceTypes(name) VALUES ('phone'), ('tablet');
+    INSERT INTO DeviceTypes(Name) VALUES ('phone'), ('tablet');
 END
 
 IF OBJECT_ID('Devices') IS NULL
 BEGIN
     CREATE TABLE Devices(
-        id INT PRIMARY KEY IDENTITY(1,1),
-        name VARCHAR(250) NOT NULL,
-        manufacturer VARCHAR(250),
-        deviceTypeId INT FOREIGN KEY REFERENCES DeviceTypes(id),
-        operatingSystem VARCHAR(250),
-        osVersion VARCHAR(50),
-        processor VARCHAR(250),
-        ramAmount INT,
-        description VARCHAR(MAX),
-        currentUserId INT NULL FOREIGN KEY REFERENCES Users(id)
+        Id INT PRIMARY KEY IDENTITY(1,1),
+        Name VARCHAR(250) NOT NULL,
+        Manufacturer VARCHAR(250),
+        DeviceTypeId INT FOREIGN KEY REFERENCES DeviceTypes(Id),
+        OperatingSystem VARCHAR(250),
+        OsVersion VARCHAR(50),
+        Processor VARCHAR(250),
+        RamAmount INT,
+        Description VARCHAR(MAX),
+        CurrentUserId INT NULL FOREIGN KEY REFERENCES Users(Id)
     );
 END
